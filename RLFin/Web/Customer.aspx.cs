c7167300845f7192d12f0b1e2dcaa636c9691811
lufeng <lufeng@lufeng.com>
2018-06-20 13:38:31 +0800

    客户 厂商

---
+++ RLFin/Web/Customer.aspx.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using RLFin.Common;
+using RLFin.Models;
+
+namespace RLFin.Web
+{
+    public partial class Customer : LocalPage
+    {
+        /// <summary>
+        /// 页面加载
+        /// </summary>
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                #region 验证权限
+
+                #endregion
+
+                #region 获取参数
+
+                #endregion
+
+                //初始化
+                this.Initialize();
+            }
+
+            #region 页面标题
+
+            this.Title = PageTitle.Text;
+
+            #endregion
+
+            #region 页面要素
+
+
+            #endregion
+        }
+
+        #region 初始化
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        private void Initialize()
+        {
+            #region 页面内容
+
+            #endregion
+
+            //绑定列表
+            //this.BindList();
+        }
+
+        #endregion
+
+        #region 绑定数据
+
+        /// <summary>
+        /// 绑定列表
+        /// </summary>
+        private void BindList()
+        {
+            using (ContractProvider contProvider = new ContractProvider())
+            {
+                List.DataSource = contProvider.GetCustomerByName(CustName.Text.Trim());
+            }
+            List.DataBind();
+        }
+
+        /// <summary>
+        /// 行绑定
+        /// </summary>
+        protected void List_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            switch (e.Row.RowType)
+            {
+                //数据行
+                case DataControlRowType.DataRow:
+                    #region 数据绑定
+
+                    ////数据
+                    //string rowStatus = ((DataRowView)e.Row.DataItem)["Status"].ToString().Trim();
+
+                    ////控件
+                    //HtmlInputCheckBox rowCheckControl = (HtmlInputCheckBox)e.Row.FindControl("RowCheck");
+
+                    ////指派
+                    //rowCheckControl.Disabled = (rowStatus == "S");
+
+                    #endregion
+                    break;
+
+                case DataControlRowType.EmptyDataRow:
+                case DataControlRowType.Header:
+                case DataControlRowType.Separator:
+                case DataControlRowType.Pager:
+                case DataControlRowType.Footer:
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// 翻页
+        /// </summary>
+        protected void List_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            List.PageIndex = e.NewPageIndex;
+            this.BindList();
+        }
+
+        #endregion
+
+        #region 操作
+
+        /// <summary>
+        /// 确定
+        /// </summary>
+        protected void OKButton_Click(object sender, EventArgs e)
+        {
+            this.BindList();
+        }
+
+        /// <summary>
+        /// 取消
+        /// </summary>
+        protected void CancelButton_Click(object sender, EventArgs e)
+        {
+            this.Initialize();
+        }
+
+        /// <summary>
+        /// 删除
+        /// </summary>
+        protected void DeleteButton_Click(object sender, EventArgs e)
+        {
+            bool deleted = false;
+
+            try
+            {
+                using (ContractProvider contProvider = new ContractProvider())
+                {
+                    foreach (GridViewRow row in List.Rows)
+                    {
+                        HtmlInputCheckBox rowCheckControl = (HtmlInputCheckBox)row.FindControl("RowCheck");
+                        if (rowCheckControl.Checked)
+                        {
+                            string custNo = List.DataKeys[row.RowIndex]["rccust"].ToString().Trim();
+
+                            contProvider.DeleteCustomer(custNo);
+
+                            //有项被删除
+                            deleted = true;
+                        }
+                    }
+                }
+            }
+            catch (Exception error)
+            {
+                this.ShowErrorMessage(this.GetGlobalResourceString("DeleteErrorMessage") + error.Message);
+                return;
+            }
+
+            if (deleted)
+            {
+                this.BindList();
+            }
+            else
+            {
+                this.ShowInfoMessage(this.GetGlobalResourceString("NotSelectMessage"));
+            }
+        }
+
+        #endregion
+    }
+}
